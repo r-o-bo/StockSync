7be73f5fd22903eb7b5554a1ef60005248bbaab9
r-o-bo <ther0b0tombstone@gmail.com>
2025-07-31 00:34:25 +0530

    checked rate limits

--- config/api_config.py
+++ config/api_config.py
@@ -1,10 +1,12 @@
 import requests
 import os
+import json
 from dotenv import load_dotenv
 
 load_dotenv()
 
 API_KEY = os.getenv("API_KEY")
+
 
 base_url = "https://yahoo-finance127.p.rapidapi.com"
 
@@ -14,7 +16,40 @@ needed_headers = {
 }
 
 def check_rate_lim():
-    my_url = f"{base_url}/price"
-    res = requests.get(url=my_url,headers=needed_headers,params={"symbol": "eth-usd"})
+    symbol = "eth-usd"
+    my_url = f"{base_url}/price/{symbol}"
+    try:
+        res = requests.get(url=my_url,headers=needed_headers)
+        res.raise_for_status() 
+        stat_code = res.status_code
+    
+        daily_limits = res.headers.get('x-ratelimit-requests-limit')
+        daily_remaining = res.headers.get('x-ratelimit-requests-remaining')
+        calls_per_min_allowed = res.headers.get('X-RateLimit-Limit')
+        calls_per_min_remaining = res.headers.get('X-RateLimit-Remaining')
+
+
+        rate_limits = {
+            'daily_limit': daily_limits,
+            'daily_remaining': daily_remaining,
+            'minute_limit': calls_per_min_allowed,
+            'minute_remaining': calls_per_min_remaining
+        }
+
+        print(stat_code)
+        print(rate_limits)
+
+        return rate_limits
+    
+    except requests.exceptions.HTTPError as err:
+        print("HTTP error occurred:", err)
+        print("Response text:", res.text)
+
+        return None
+
+# check_rate_lim() call to check rate lim
+    
+
+
 
     
