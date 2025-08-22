4fd90b0c787f811d4be1c29a2996904da1c965b2
r-o-bo <ther0b0tombstone@gmail.com>
2025-08-22 23:59:53 +0530

    herhf

--- src/load.py
+++ src/load.py
@@ -1,2 +1,5 @@
 import mysql.connector
 from mysql.connector import Error
+
+def create_db_connection(host_name, user_name, user_password, db_name):
+    pass
--- src/transform.py
+++ src/transform.py
@@ -2,7 +2,7 @@ import pandas as pd
 from datetime import datetime, timezone
 
 
-# ngl minimal transformation cuz json was already part clean
+# ngl minimal transformation cuz json was already part clean 
 def transform_data(summary):
     # Coordinated Universal Time (UTC) and format it into an ISO 8601 string
     summary["transformed_at"] = datetime.now(timezone.utc).isoformat()
