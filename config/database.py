4b5bd5dc6efd312f3a1854877d1148bb480fc802
r-o-bo <ther0b0tombstone@gmail.com>
2025-08-23 02:19:45 +0530

    almost finished pipeline lord give me strength to finish this tomorrow

--- config/database.py
+++ config/database.py
@@ -1,15 +1,22 @@
 import mysql.connector
+from mysql.connector import Error
 from dotenv import load_dotenv
 import os
 
 load_dotenv()
 
-def db_connection():
-    conn = mysql.connector.connect(
-        host=os.getenv("HOST", "localhost"),
-        user=os.getenv("MYSQL_USERNAME", "root"),
-        password=os.getenv("MYSQL_PASSWORD", ""),
-        database=os.getenv("MYSQL_DATABASE", "crypto_db")
-    )
-
-    return conn
+def dreate_db_connection():
+    db_connection = None
+    try:
+        db_connection = mysql.connector.connect(
+            host=os.getenv("HOST", "localhost"),
+            user=os.getenv("MYSQL_USERNAME", "root"),
+            password=os.getenv("MYSQL_PASSWORD", ""),
+            database=os.getenv("MYSQL_DATABASE", "crypto_db")
+        )
+        if db_connection.is_connected():
+            print("Database connection successful")
+    except Error as e:
+        print(f"[DATABASE CONNECTION ERROR]: {e}")
+    
+    return db_connection
--- src/load.py
+++ src/load.py
@@ -1,5 +1,31 @@
 import mysql.connector
 from mysql.connector import Error
+from database import db_connection
 
-def create_db_connection(host_name, user_name, user_password, db_name):
-    pass
+def create_table():
+    CREATE_TABLE_SQL_QUERY = ("""
+        CREATE TABLE IF NOT EXISTS eth_prices (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            symbol VARCHAR(10),
+            name VARCHAR(50),
+            price DECIMAL(18,8),
+            change DECIMAL(18,8),
+            percent_change VARCHAR(10),
+            volume BIGINT,
+            market_cap BIGINT,
+            week_high DECIMAL(18,8),
+            week_low DECIMAL(18,8),
+            logo VARCHAR(255),
+            last_updated VARCHAR(50),
+            transformed_at DATETIME
+        )
+    """)
+    try:
+        cursor = db_connection.cursor()
+        cursor.execute(CREATE_TABLE_SQL_QUERY)
+        db_connection.commit()
+        print("Table created successfully")
+
+    except Error as e:
+        print(f"[CREATING TABLE ERROR]: '{e}'")
+  
